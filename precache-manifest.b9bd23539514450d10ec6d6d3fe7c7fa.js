3b0e883591cb6dee71d8fae08f20fc1ab12436da
mikatalk <mika.indie@gmail.com>
2018-06-21 17:42:04 -0700

    Import main style and fix ol padding

---
+++ precache-manifest.b9bd23539514450d10ec6d6d3fe7c7fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "31f80db3f52261f08529",
+    "url": "https://mikatalk.github.io/fsi/js/chunk-vendors.31f80db3.js"
+  },
+  {
+    "revision": "bdb75f090b99f56f0c40",
+    "url": "https://mikatalk.github.io/fsi/js/app.bdb75f09.js"
+  },
+  {
+    "revision": "d46eeda5d387fd035e062840e9fb79e4",
+    "url": "https://mikatalk.github.io/fsi/index.html"
+  },
+  {
+    "revision": "bdb75f090b99f56f0c40",
+    "url": "https://mikatalk.github.io/fsi/css/app.2edc5560.css"
+  }
+];
